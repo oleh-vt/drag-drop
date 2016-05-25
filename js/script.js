92648669fbff24360a8dbef7c05d442c7d50dc8b
olezhka <jewell876@gmail.com>
2016-05-26 01:17:21 +0200

    drag&drop added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
-$(function (){
-    $.getJSON('http://young.org.ua/test.json', function (data) {
-        for(var key in data) {
-            $('#category').append($('<option>', {
-                value: key,
-                text: key
-            }));
-        }
+$(document).ready(function(){
+    $.getJSON('http://young.org.ua/test.json', foo);
+
+    // $('#droppable').droppable({
+    //     drop: dropHandler
+    // });
+    $('#droppable').droppable({
+        drop: dropHandler
     });
 
-    $('.draggable-item').draggable({
-        helper: 'clone',
-        revert: 'invalid'
+    $('#category').on('change', function(){
+        $('option:disabled').remove();
+        $('ul').empty();
+        $.getJSON('http://young.org.ua/test.json', function(data){
+            var arr = data[$('#category').val()];
+            var draggable = $('#draggable');
+            for(var i = 0; i < arr.length; i++){
+                $(draggable).append($('<li>',{text: arr[i]}).addClass('draggable-item'));
+            }
+            $('.draggable-item').draggable({
+                helper: 'clone',
+                revert: 'invalid',
+                cursor: 'move'
+            });
+        });
     });
-
-    $('ul, li').disableSelection();
 });
+
+function foo(data){
+    var categorySelection = $('#category');
+    for(var key in data){
+        $(categorySelection).append($('<option>', {
+            value: key,
+            text: key
+        }));
+    }
+}
+
+function dropHandler(event, ui){
+    var draggableItem = ui.draggable;
+    $('#droppable').append($('<li>',{text: $(draggableItem).text()}).addClass('accepted-item'));
+}
